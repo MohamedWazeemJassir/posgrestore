5aa4b15e1685cca857bf3464203aa8c16dc61182
wazeem <mohamedwazeemjassir@gmail.com>
2025-08-30 23:42:12 +0530

    Add updateProduct and fetchProduct

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useProductStore } from "../store/useProductstore";
+import { useNavigate, useParams } from "react-router-dom";
 
 function ProductPage() {
+  const {
+    currentProduct,
+    formData,
+    setFormData,
+    loading,
+    error,
+    fetchProduct,
+    updateProduct,
+    deleteProduct,
+  } = useProductStore();
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchProduct(id)
+  },[fetchProduct, id])
+  console.log(currentProduct);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="loading loading-spinner loading-lg"/>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <div className="alert alert-error">{error}</div>
+      </div>
+    );
+  }
+
   return (
-    <div>ProductPage</div>
-  )
+  <div>
+    
+  </div>)
 }
 
-export default ProductPage
+export default ProductPage;
